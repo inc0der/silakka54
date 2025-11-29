e7a0ee01aac5853951972a93da81a9616506b563
inc0der <inc0der.dev@gmail.com>
2025-11-29 11:27:42 -0400

    update default keymap to closer resemble my default vial keymap config

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -6,19 +6,19 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LGUI, MO(1), KC_SPC,           KC_ENT,  KC_RCTL,  KC_RALT
+        KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LCTRL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                            KC_LGUI, MO(1), KC_SPC,               KC_ENT,  KC_BSPC,  MO(2)
     ),
     [1] = LAYOUT(
-        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_PGUP, KC_PGDN, KC_HOME, KC_END,  KC_DEL,  KC_F12,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LBRC, KC_RBRC,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                            KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
-    )
+        KC_F11,   KC_F1,     KC_F2,    KC_F3,       KC_F4,       KC_F5,                        KC_F6,  KC_F7,   KC_F8,    KC_F9,    KC_F11,   KC_F12,
+        KC_TRNS,  S(KC_1),   S(KC_2),  KC_LBRC,     KC_RBRC,     S(KC_BSLS),                   KC_P7,  KC_P8,   KC_P9,    KC_PPLS,  KC_PMNS,  KC_UP,
+        KC_TRNS,  S(KC_4),   S(KC_4),  S(KC_9),     S(KC_0),     S(KC_UNDS),                   KC_P4,  KC_P5,   KC_P6,    KC_PCMM,  KC_PAST,  KC_DOWN,
+        KC_TRNS,  S(KC_5),   S(KC_6),  S(KC_LBRC),  S(KC_RBRC),  S(KC_GRV),                    KC_P1,  KC_P2,   KC_P3,    KC_EQL,   KC_LEFT,  KC_RGHT,
+                                                         KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,  KC_TRNS,     KC_TRNS
+    ),
 };
 
 
